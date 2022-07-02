3018f9ff445281b898ba641b7188adb4a554b655
Ahmad <ahmad.medoo.700@gmail.com>
2022-07-02 15:06:00 +0300

    rebuild register done

--- src/app/main/register/register.component.ts
+++ src/app/main/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private api:ApiService,private router:Router) { }
+  loading = false;
+  form = new FormGroup({
+    username: new FormControl(null, [Validators.required,Validators.minLength(6),Validators.maxLength(32)]),
+    password: new FormControl(null, [Validators.required,Validators.minLength(4),Validators.maxLength(32)]),
+    email:new FormControl(null,[Validators.required,Validators.email])
+  }) 
+  serverResponse = '';
+  async register() {
+    if (this.form.valid) {
+      this.loading = true;
+      await this.api.register(this.form.value)
+        .then(() => this.router.navigate(['/']))
+        .catch((e) => { this.serverResponse = e.message || 'Something Went Wrong in server' })
+        .finally(() => { this.loading = false;  })
+    
+    }
   }
 
 }
