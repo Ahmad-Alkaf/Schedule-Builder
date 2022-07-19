571f6f672487238af6b736dc54da7bd29e46ab1c
Ahmad <ahmad.medoo.700@gmail.com>
2022-07-19 17:05:48 +0300

    done themes and dark mode toggle

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,3 +1,4 @@
+import { ɵparseCookieValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '@service/api.service';
 import { BehaviorSubject } from 'rxjs';
@@ -32,22 +33,36 @@ export class HeaderComponent implements OnInit{
   /**
    * array of css classes that toggle in <body> to set the theme
    */
-  public themes: string[] = [
-    'light-theme-indigo',
-    'dark-theme-purple',
+  public themes: string[] = [//todo make toggle button for dark/light mode. while the themes are just the color. An idea to do that is by making each theme has dark and light classes and files. So, themes arr will be without dark/light i.e('theme-purple' instead of 'dark-theme-purple') and then based on isDark append dark/light string into arr item. Ex:$('body').addClass(isDark == true ? 'dark-'+arr[i] : 'light-'+arr[i]);
+    'theme-indigo',
+    'theme-purple',
+    'theme-yellow'
   ];
+  public isDark = false;
   public selectedTheme: BehaviorSubject<string> = new BehaviorSubject<string>(this.themes[0]);
   ngOnInit() {
-    $('body').addClass(this.selectedTheme.value);
+    this.invokePreference();
     this.selectedTheme.subscribe({
       next: (v) => {
-        for (let t of this.themes)
-          $('body').removeClass(t);
-        $('body').addClass(v);
-    }})
+        for (let t of this.themes) {
+          $('body').removeClass('dark-'+t);
+          $('body').removeClass('light-'+t);
+        }
+        $('body').addClass((this.isDark ? 'dark-' : 'light-') + v);
+        this.savePreference();
+      }
+    })
   }
   
-  changeTheme(event: any) {
-    
+  savePreference() {
+    console.error('save',{isDark:this.isDark,theme:this.selectedTheme.value})
+    localStorage.setItem('isDark', JSON.stringify(this.isDark));
+    localStorage.setItem('theme', this.selectedTheme.value);
   }
+  invokePreference():void {
+    this.isDark = JSON.parse(localStorage.getItem('isDark') || 'false');
+    this.selectedTheme.next(localStorage.getItem('theme') || this.themes[0]);
+    console.error({isDark:this.isDark,theme:this.selectedTheme.value})
+  }
+  
 }
